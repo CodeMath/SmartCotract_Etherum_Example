72404035d4a65f226a693b050ee11f24c9a22d48
Codemath <xyrho123@me.com>
2018-04-02 17:10:22 +0900

    Feat: Change the concept of service
     - change to Crowdsale.

--- index/urls.py
+++ index/urls.py
@@ -3,4 +3,5 @@ from index import views
 
 urlpatterns = [
     path('', views.index),
+    path('logout/', views.logout, name="logout"),
 ]
--- index/views.py
+++ index/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
 
-# Create your views here.
+
+
 def index(request):
 
     return render(request,'index.html')
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
